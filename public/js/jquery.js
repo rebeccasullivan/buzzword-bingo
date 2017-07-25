6e899a0b6b99298c91945695982ed29187311636
Rebecca <rhhsullivan@gmail.com>
2017-07-25 16:34:38 -0400

    Added free button marked upon game startup - need to fix so that it still appears upon new card generation

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -6,14 +6,15 @@ $(document).ready(function() {
 	function fillFreeSquare() {
 		console.log("Entered 'fillFreeSquare method");
 		var $bingoGrid = $('#bingoSquare').find('.table');
-		
 		var $middleRow = $bingoGrid.find('tr:nth-child(3)');
-		console.log($middleRow);
-		
 		var $middleSpace = $middleRow.find('td:nth-child(3)');
-		console.log($middleSpace);
-		
 		var $middleDiv = $middleSpace.find('div');
 		$middleDiv.html("FREE!");
+		$middleDiv.addClass()
+		
+		var $checkmarkImg = $middleSpace.find('img');
+		$checkmarkImg.toggle();
 	}
+	
+	
 });
