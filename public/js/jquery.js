5111a727859916032ead2bbd24cb8fee4ec369dc
Rebecca <rhhsullivan@gmail.com>
2017-07-30 11:06:04 -0400

    Fixed bug in row and column winning logic

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
 		var $bingoGrid = $('#bingoCard').find('.table');
 		var $middleRow = $bingoGrid.find('tr:nth-child(3)');
 		var $middleSpace = $middleRow.find('td:nth-child(3)');
+		$middleSpace.addClass('checked');
 		var $middleDiv = $middleSpace.find('div');
 		$middleDiv.html("FREE!");
-		$middleDiv.addClass()
 		
 		var $checkmarkImg = $middleSpace.find('img');
 		$checkmarkImg.toggle();
@@ -32,28 +32,31 @@ $(document).ready(function() {
 	function addMark(caller) {
 		var $checkmarkImg = $(caller).find('img');
 		$checkmarkImg.toggle();
-		$checkmarkImg.parent('td').addClass('checked');
+		$checkmarkImg.parent('td').toggleClass('checked');
 	}
 	
 	function evaluateRows() {
 		var $bingoGrid = $('#bingoCard').find('.table');
 		var $rows = $bingoGrid.find('> tbody > tr');
+		console.log($rows);
+		console.log('$Rows length: ' + $rows.length);
 		var hasWon = false;
 		
-		for (i = 0; i < $rows.length; i++) {
-			if(evaluateRow($rows[i])) {
+		for (var i = 0; i < $rows.length; i++) {
+			if (isWinningRow($rows[i])) {
 				hasWon = true;
 			}
 		}
 		return hasWon;
 	}
 	
-	function evaluateRow(row) {
-		// Return a bool of whether all 5 are checked
+	function isWinningRow(row) {
+		// Determines whether all 5 squares in row are checked
 		var $squares = $(row).find('td');
 		var winningRow = true;
 		
-		for (i = 0; i < $squares.length; i++) {
+		for (var i = 0; i < $squares.length; i++) {
+			console.log($($squares[i]));
 			if (!$($squares[i]).hasClass('checked')) {
 				winningRow = false;
 			}
@@ -68,26 +71,26 @@ $(document).ready(function() {
 		var hasWon = false;
 		
 		// Create columns
-		for (i = 0; i < $rows.length; i++) {
+		for (var i = 0; i < $rows.length; i++) {
 			var $squares = $($rows[i]).find('td');
 			
-			for (j = 0; j < $squares.length; j++) {
+			for (var j = 0; j < $squares.length; j++) {
 				$columns[j].push($squares[j]);
 			}
 		}
 		
-		for (i = 0; i < $columns.length; i++) {
-			if (evaluateColumn($columns[i])) {
+		for (var i = 0; i < $columns.length; i++) {
+			if (isWinningColumn($columns[i])) {
 				hasWon = true;
 			}
 		}
 		return hasWon;
 	}
 	
-	function evaluateColumn(column) {
+	function isWinningColumn(column) {
 		var winningColumn = true;
 		
-		for (i = 0; i < column.length; i++) {
+		for (var i = 0; i < column.length; i++) {
 			if (!$(column[i]).hasClass('checked')) {
 				winningColumn = false;
 			}
