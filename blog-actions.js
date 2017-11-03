a21936485a2e6ab58ba8ba5e9732a9b6374ea17f
Rebecca <rhhsullivan@gmail.com>
2017-11-02 22:10:37 -0400

    Started to implement blog search

---
+++ blog-actions.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$('#search-btn').click(function() {
+		
+	});
+	
+	$('.search').on('keyup', function(e) {
+		if(e.keyCode === 13) {
+			
+		}
+	});
+	
+	function searchContentful(searchText) {
+		
+	}
+	
+});
