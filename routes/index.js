460e6b44654e671a5a73507e753346c5832d8bd3
Rebecca <rhhsullivan@gmail.com>
2017-11-05 13:41:32 -0500

    Added search functionality

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,18 @@ var Handlebars = require('hbs');
 var router = express.Router();
 var pageTitle = "Rebecca Sullivan";
 var request = require('superagent');
-var Cosmic = require('cosmicjs');
 var contentful = require('contentful');
 var config = require('../package.json').config || {};
 var marked = require('marked');
 var redis = require('redis');
+var bodyParser = require('body-parser');
 var REDIS_PORT = process.env.REDIS_PORT;
 
 var redisClient = redis.createClient(REDIS_PORT);
 var contentfulBlogKey = 'contentful-blog-posts';
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: false}));
 
 // Basic GET routes for all pages
 router.get('/', function (req, res) {
@@ -55,9 +58,8 @@ function cache(req, res, next) {
 
     redisClient.get(contentfulBlogKey, function (err, entries) {
         if (entries != null) {
-
             var jsonEntries = JSON.parse(entries);
-            console.log(jsonEntries);
+
             var tags = new Set();
             var blogPosts = [];
         		jsonEntries.items.forEach(function (entry) {
@@ -67,25 +69,29 @@ function cache(req, res, next) {
         			});
         	  });
 
-        		let tagArray = Array.from(tags);
+        		var tagArray = Array.from(tags);
+            console.log(tagArray);
+
             res.render('blog', {
                 blogPosts: blogPosts,
-                tags: tags
-              });
+                tags: tagArray
+            });
         } else {
             next();
         }
     });
 }
 
-function getBlogPosts(req, res, next) {
-  // Get Contentful client
-  var contentfulClient = contentful.createClient({
+function getContentfulClient() {
+  return contentful.createClient({
     accessToken: config.accessToken,
     space: config.space
   });
+}
 
-  var blogPosts = [];
+function getBlogPosts(req, res, next) {
+  console.log('in getBlogPosts function in index.js');
+  var contentfulClient = getContentfulClient();
 
 	// Get all Contentful entries
 	contentfulClient.getEntries({
@@ -93,6 +99,7 @@ function getBlogPosts(req, res, next) {
 		order: '-sys.createdAt'
 	}).then(function (entries) {
 
+    var blogPosts = [];
     var tags = new Set();
 
 		entries.items.forEach(function (entry) {
@@ -105,6 +112,7 @@ function getBlogPosts(req, res, next) {
 		let tagArray = Array.from(tags);
     var jsonStringEntries = JSON.stringify(entries);
 
+    // Set cache entry with all blog entries
     redisClient.setex(contentfulBlogKey, 60, jsonStringEntries);
 
     // Send entries to handlebars template to display
@@ -118,20 +126,52 @@ function getBlogPosts(req, res, next) {
 // Blog Routes
 router.get('/blog', cache, getBlogPosts);
 
+router.get('/search', function(req, res) {
+  var searchText = req.query.searchText;
+  console.log('Search text: ' + searchText);
+
+  var contentfulClient = getContentfulClient();
+  contentfulClient.getEntries({
+    'query': searchText
+  })
+  .then(function (entries) {
+    console.log('entries.items: ' + entries.items);
+    console.log('entries.items.length: ' + entries.items.length);
+    var blogPosts = [];
+    var tags = new Set();
+
+		entries.items.forEach(function (entry) {
+			blogPosts.push(entry);
+			entry.fields.tags.forEach(function (tag) {
+				tags.add(tag);
+			});
+	  });
+
+		let tagArray = Array.from(tags);
+    res.render('search', {
+	  		blogPosts: blogPosts,
+			  tags: tagArray
+	  });
+  }).catch(function (reason) {
+    console.log(reason);
+  });
+});
+
 router.get('/blog/:postId/:entrySlug', function (req, res) {
 	// Make API call to search for blog post
 	var entrySlug = req.params.entrySlug;
 	var postId = req.params.postId;
 
-	client.getEntry(postId).then(function (entry) {
-		entry.bodyHtml = marked(entry.fields.bodyCopy)
+  var contentfulClient = getContentfulClient();
+	contentfulClient.getEntry(postId).then(function (entry) {
+		entry.bodyHtml = marked(entry.fields.bodyCopy);
 
 		// Render blog layout with specific blog post elements
 		res.render('blog-post', {
 			post: entry
-		})
-	})
-})
+		});
+	});
+});
 
 // Logic for getting seed data for Bingo Squares from SQLite
 router.get('/bingo-squares', function (req, res) {
